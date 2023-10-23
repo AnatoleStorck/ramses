2897e82c9f083a8bcddccf129ba18b9b56eb3508
Anatole Storck <storckanatole@gmail.com>
2023-10-23 16:43:58 +0200

    nevermind this was important

--- pm/particle_snapshot.f90
+++ pm/particle_snapshot.f90
@@ -45,6 +45,31 @@ contains
 
  end function binary_search
 
+ recursive subroutine quicksort(a, first, last)
+    integer(i8b), intent(inout), dimension(:) :: a
+    integer(i8b) :: x, t
+    integer, intent(in) :: first, last
+    integer :: i, j
+
+    x = a( (first+last) / 2 )
+    i = first
+    j = last
+    do
+       do while (a(i) < x)
+          i=i+1
+       end do
+       do while (x < a(j))
+          j=j-1
+       end do
+       if (i >= j) exit
+       t = a(i);  a(i) = a(j);  a(j) = t
+       i=i+1
+       j=j-1
+    end do
+    if (first < i-1) call quicksort(a, first, i-1)
+    if (j+1 < last)  call quicksort(a, j+1, last)
+ end subroutine quicksort
+
   subroutine read_particle_ids(particle_source_file, n, ids, particle_type)
     character(len=*), intent(in) :: particle_source_file
     integer, intent(out) :: n
